3029e32385da36b49a35bd0bd8e14df68971dcf0
Clotilde Connault <clotilde.connault@gmail.com>
2021-01-14 11:38:46 +0100

    labo 2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,73 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DetailComponent } from './films/detail/detail.component';
+import { GenreFormComponent } from './films/detail/genre-form/genre-form.component';
+import { FilmsComponent } from './films/films.component';
+import { FormulaireComponent } from './films/formulaire/formulaire.component';
+import { PersonFormComponent } from './films/person-form/person-form.component';
+import { UpdateComponent } from './films/update/update.component';
+import { IntervenantComponent } from './intervenant/intervenant.component';
+import { PersDetailComponent } from './intervenant/pers-detail/pers-detail.component';
+import { PersFormComponent } from './intervenant/pers-form/pers-form.component';
+import { PersUpdateComponent } from './intervenant/pers-update/pers-update.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:'',
+    component: FilmsComponent,
+    children: [{
+    path: 'detail/:id',
+    component: DetailComponent,
+    children: [{
+      path: 'detail/add/:id',
+      component: GenreFormComponent
+    }]
+    },
+  {
+  path: 'ajout',
+  component: FormulaireComponent
+  },
+ 
+  {
+  path: 'update/:id',
+  component: UpdateComponent
+  },
+
+  {
+    path: 'intervenants/:id',
+    component: PersonFormComponent
+    },
+
+  ],
+  runGuardsAndResolvers: 'always'
+
+},
+
+{
+  path: 'films',
+  component: FilmsComponent
+},
+
+{
+  path: 'int',
+  component: IntervenantComponent,
+    children: [{
+      path: 'int/detail/:id',
+      component: PersDetailComponent
+      },
+    {
+    path: 'int/ajout',
+    component: PersFormComponent
+    },
+   
+    {
+    path: 'int/update/:id',
+    component: PersUpdateComponent
+    }]
+}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {onSameUrlNavigation: 'reload'})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,52 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FilmsComponent } from './films/films.component';
+import { HttpClientModule } from '@angular/common/http';
+import { DetailComponent } from './films/detail/detail.component';
+import { registerLocaleData } from '@angular/common';
+import localeFr from '@angular/common/locales/fr';
+import { FormulaireComponent } from './films/formulaire/formulaire.component';
+import { FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { UpdateComponent } from './films/update/update.component';
+import { PersonFormComponent } from './films/person-form/person-form.component';
+import { IntervenantComponent } from './intervenant/intervenant.component';
+import { PersFormComponent } from './intervenant/pers-form/pers-form.component';
+import { PersUpdateComponent } from './intervenant/pers-update/pers-update.component';
+import { PersDetailComponent } from './intervenant/pers-detail/pers-detail.component';
+import { GenreFormComponent } from './films/detail/genre-form/genre-form.component';
+registerLocaleData(localeFr, 'fr');
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FilmsComponent,
+    DetailComponent,
+    FormulaireComponent,
+    UpdateComponent,
+    PersonFormComponent,
+    IntervenantComponent,
+    PersFormComponent,
+    PersUpdateComponent,
+    PersDetailComponent,
+    GenreFormComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    FormsModule
+    
   ],
-  providers: [],
+  providers: [
+    {
+      provide: LOCALE_ID,
+      useValue: 'fr'
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
